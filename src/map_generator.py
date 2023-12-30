1a5712f4ff6b0a443315b8b46eaf0f01b0e95e7e
Mark <mgnorrish@gmail.com>
2023-12-31 00:52:37 +1100

    temple generator, good lights

--- src/map_generator.py
+++ src/map_generator.py
@@ -1,7 +1,7 @@
 import base64
 import io, os
 from PIL import Image, ImageDraw
-from shapely.geometry import Point, Polygon
+from shapely.geometry import MultiPolygon, Point, Polygon
 import math, random, simplejson
 from scipy.spatial import Delaunay
 from scipy.sparse.csgraph import minimum_spanning_tree
@@ -108,7 +108,7 @@ def build_path(start, end, params={}):
 # so that eg I could have a cave system
 def build_forest(params={}):
   sx,sy = size = params.get("size", (500,500))
-  density = params.get("density", 1)
+  density = params.get("density", 0.0001)
   tree_thickness = params.get("tree-thick", 1.8)
   grass_color = (75,255,55)
   brown = (130, 130, 0)
@@ -120,26 +120,26 @@ def build_forest(params={}):
   ppg = params.get("pix",1) # pixels per grid unit
   oosqrt2 = 2**-0.5
   # We maintain a map of buckets, each containing some trees, a list of (x,y,species_ix, remaining_lifespan). This makes it faster to check for collisions.
-  bucket_length = 100
-  buckets = {(xb,yb):[] for xb in range(sx//bucket_length+1) for yb in range(sy//bucket_length+1)} # bucket coords to list of tree coords
+  bucket_width = 100
+  buckets = {(xb,yb):[] for xb in range(sx//bucket_width+1) for yb in range(sy//bucket_width+1)} # bucket coords to list of tree coords
   trees = dict() # tree coords to tree data
   # Initialise with a few different species of tree.
   num_species = 2
   species_data = [Species(80+50*i, 5+5*i, 5, (30, 150, 10), (20,40,5)) for i in range(num_species)]
   # add trees
-  for i in range(int(density*sx*sy/10000)):
+  for i in range(int(density*sx*sy)):
     species = random.choice(species_data)
     new_tree = Tree(species)
     px, py = random.randint(0,sx), random.randint(0,sy)
-    xb, yb = px // bucket_length, py // bucket_length
+    xb, yb = px // bucket_width, py // bucket_width
     # confirm it can spawn there: is it within bounds; and for its new bucket pom 1 in each drn, there's nothing where dist (t, new_t) < t.rad + new_t.rad; and if I later add streams, it's not inside one
-    overlaps = [(xo,yo) for x in {max(0,xb-1),xb,min(sx//bucket_length,xb+1)} for y in {max(0,yb-1),yb,min(sy//bucket_length,yb+1)} for xo,yo in buckets[(x,y)] if is_overlap(new_tree,px,py,trees[(xo,yo)], xo,yo, margin=tree_thickness)]
+    overlaps = [(xo,yo) for x in {max(0,xb-1),xb,min(sx//bucket_width,xb+1)} for y in {max(0,yb-1),yb,min(sy//bucket_width,yb+1)} for xo,yo in buckets[(x,y)] if is_overlap(new_tree,px,py,trees[(xo,yo)], xo,yo, margin=tree_thickness)]
     if not overlaps:
       trees[(px, py)] = new_tree
       buckets[(xb, yb)] += [(px, py)]
 
   # add a clearing
-  x,y = random.randint(2,sx//bucket_length-2),random.randint(2,sy//bucket_length-2)
+  x,y = random.randint(2,sx//bucket_width-2),random.randint(2,sy//bucket_width-2)
   for x1 in range(x-2,x+3):
     for y1 in range(y-2, y+3):
       for coord in buckets[x1,y1]:
@@ -161,10 +161,10 @@ def build_forest(params={}):
 
 def build_cave(params):
   # TODO much of this is shared boilerplate from build-forest and should be factored out
-  # TODO add a pathway from a room to outside. 
   sx,sy = size = params.get("size", (5000,5000))
   floor_color = (155,155,155)
   wall_color=(50,40,30)
+  water_color = (0,0,255)
   brown = (130, 130, 0)
   out = Image.new("RGB", size, brown)
   d = ImageDraw.Draw(out)
@@ -251,9 +251,9 @@ def build_cave(params):
   flooded_room_ixes = [r for r in range(len(rooms)) if random.random() < params.get("flood",0.5)]
   flooded_path_ixes = [(i,j) for (i,j) in path_indices if i in flooded_room_ixes and j in flooded_room_ixes]
   for i in flooded_room_ixes:
-    d.polygon(rooms[i],fill=(0,0,255))
+    d.polygon(rooms[i],fill=water_color)
   for (i,j) in flooded_path_ixes:
-    d.polygon(path_coords[(room_centres[i],room_centres[j])],fill=(0,0,255))
+    d.polygon(path_coords[(room_centres[i],room_centres[j])],fill=water_color)
   lights = []
   for i in range(len(rooms)):
     if not i in flooded_room_ixes:
@@ -266,15 +266,129 @@ def build_cave(params):
   line_of_sight.append([{"x":0,"y":0},{"x":0,"y":sy/ppg},{"x":sx/ppg,"y":sy/ppg},{"x":sx/ppg,"y":0},{"x":0,"y":0}])
   return out, line_of_sight, lights
 
+def build_temple(params):
+  sx,sy = size = params.get("size", (5000,5000))
+  floor_color = (155,155,155)
+  grass_color = (75,255,55)
+  wall_color=(50,40,30)
+  brown = (130, 130, 0)
+  out = Image.new("RGB", size, brown)
+  d = ImageDraw.Draw(out)
+  border = 50
+  wall_thick=25
+  d.rectangle([(border, border), (sx-border,sy-border)], fill=grass_color)
+  line_of_sight = []
+  room_rad = params.get("rr",200)
+  sepsq = params.get("sep-sq", 500**2)
+  ppg = params.get("pix",1) # pixels per grid unit
+  
+  # Temple complexes are supposed to be akin to cave systems, but somewhat different.
+  # There are rooms, some with corridors. Outside are trees and grass.
+
+  room_centres = []
+  while len(room_centres) < (params.get("n-rooms", 5)):
+    rx,ry = (random.randint(room_rad, sx - room_rad), random.randint(room_rad, sy - room_rad))
+    if all([(rx-x)**2+(ry-y)**2 > sepsq for (x,y) in room_centres]):
+      room_centres.append((rx,ry))
+    
+  rooms = []
+  for (x,y) in room_centres:
+    n_pts = random.choice([4,5,6,8])
+    room=[]
+    t0 = 2*math.pi*random.random()
+    for i in range(n_pts):
+      theta = 2*math.pi*i/n_pts+t0
+      x1,y1 = x+room_rad*math.cos(theta), y+room_rad*math.sin(theta)
+      room.append((x1,y1))
+    room.append(room[0])
+    rooms.append(room)
+
+  # handle corridors
+  # first, create delaunay triangulation
+  simplices = Delaunay(room_centres).simplices
+  simplices = [sorted(s) for s in simplices]
+  path_indices = {(i,j) for simplex in simplices for i in simplex for j in simplex if i<j}
+  # second, delete all long edges of obtuse triangles
+  def distsq(a,b):
+    (x1,y1)=room_centres[a]
+    (x2,y2)=room_centres[b]
+    return (x1-x2)**2+(y1-y2)**2
+  def remove_long_edge(s):
+    s.sort()
+    d0=distsq(s[1],s[2])
+    d1=distsq(s[0],s[2])
+    d2=distsq(s[0],s[1])
+    if d0>d1+d2:
+      path_indices.discard((s[1],s[2]))
+    if d1>d0+d2:
+      path_indices.discard((s[0],s[2]))
+    if d2>d0+d1:
+      path_indices.discard((s[0],s[1]))
+  for s in simplices:
+    remove_long_edge(s)
+
+  path_indices = {p for p in path_indices if random.random()<0.5}
+  path_coords = {(room_centres[i],room_centres[j]) for (i,j) in path_indices}
+
+  path_coords = {(s,e):build_path(s,e,{"kinks":0,"width":140}) for (s,e) in path_coords}
+
+  all_areas = Polygon([])
+  for poly in list(path_coords.values())+rooms:
+    all_areas = all_areas.union(Polygon(poly))
+  all_areas = MultiPolygon([all_areas])
+
+  lights = []
+  for i in range(len(rooms)):
+    lights.append(room_centres[i])
+  for poly in all_areas.geoms:
+    outlines = list(poly.interiors)+[poly.exterior]
+    for p in outlines:
+      d.polygon(p.coords,fill=floor_color)
+      d.line(p.coords,fill=wall_color,width=wall_thick)
+      for x,y in p.coords:
+        d.ellipse([(x-wall_thick/2,y-wall_thick/2),(x+wall_thick/2,y+wall_thick/2)],fill=wall_color)
+      line_of_sight.append([{"x":(x/ppg),"y":(y/ppg)} for (x,y) in p.coords])
+    line_of_sight.append([{"x":0,"y":0},{"x":0,"y":sy/ppg},{"x":sx/ppg,"y":sy/ppg},{"x":sx/ppg,"y":0},{"x":0,"y":0}])
+
+  # add trees outside
+  density = 3e-5
+  bucket_width = 100
+  buckets = {(xb,yb):[] for xb in range(sx//bucket_width+1) for yb in range(sy//bucket_width+1)} # bucket coords to list of tree coords
+  trees = dict() # tree coords to tree data
+  tree_thickness = params.get("tree-thick", 1.8)
+  species_data = [Species(80+50*i, 5+5*i, 5, (30, 150, 10), (20,40,5)) for i in range(2)]
+  buffered_area = all_areas.buffer(100)
+  for i in range(int(density*sx*sy)):
+    species = random.choice(species_data)
+    new_tree = Tree(species)
+    px, py = random.randint(0,sx), random.randint(0,sy)
+    xb, yb = px // bucket_width, py // bucket_width
+    # confirm it can spawn there: is it within bounds; and for its new bucket pom 1 in each drn, there's nothing where dist (t, new_t) < t.rad + new_t.rad; and if I later add streams, it's not inside one
+    overlaps = [(xo,yo) for x in {max(0,xb-1),xb,min(sx//bucket_width,xb+1)} for y in {max(0,yb-1),yb,min(sy//bucket_width,yb+1)} for xo,yo in buckets[(x,y)] if is_overlap(new_tree,px,py,trees[(xo,yo)], xo,yo, margin=tree_thickness)]
+    if not overlaps and not Point(px,py).within(buffered_area):
+      trees[(px, py)] = new_tree
+      buckets[(xb, yb)] += [(px, py)]
+
+  oosqrt2 = 2**-0.5
+  for ((x,y),tree) in trees.items():
+    d.ellipse([(x-tree.x_rad, y-tree.y_rad), (x+tree.x_rad, y+tree.y_rad)], fill=tree.color, outline=(0,0,0))
+    line_of_sight.append([{"x":(x-tree.x_rad*oosqrt2)/ppg, "y":(y-tree.y_rad*oosqrt2)/ppg},{"x":(x+tree.x_rad*oosqrt2)/ppg, "y":(y+tree.y_rad*oosqrt2)/ppg},{"x":(x-tree.x_rad*oosqrt2)/ppg, "y":(y-tree.y_rad*oosqrt2)/ppg}])
+    line_of_sight.append([{"x":(x-tree.x_rad*oosqrt2)/ppg, "y":(y+tree.y_rad*oosqrt2)/ppg},{"x":(x+tree.x_rad*oosqrt2)/ppg, "y":(y-tree.y_rad*oosqrt2)/ppg},{"x":(x-tree.x_rad*oosqrt2)/ppg, "y":(y+tree.y_rad*oosqrt2)/ppg}])
+  line_of_sight.append([{"x":0,"y":0},{"x":0,"y":sy/ppg},{"x":sx/ppg,"y":sy/ppg},{"x":sx/ppg,"y":0},{"x":0,"y":0}])
+
+  return out, line_of_sight, lights
+
 if __name__=="__main__":
   ppg = 50
   width = 2500
-  key = ["forest", "cave"][0]
+  key = ["forest", "cave", "temple"][2]
   lights = []
   if key == "forest":
-    image, los = build_forest(params={"size":(width,width), "pix":ppg, "density":4, "tree-thick":1.8})
+    image, los = build_forest(params={"size":(width,width), "pix":ppg, "density":0.0004, "tree-thick":1.8})
   elif key == "cave":
     image, los, lights = build_cave(params={"size":(width,width), "pix":ppg, "n-rooms":9})
+  elif key == "temple":
+    image, los, lights = build_temple(params={"size":(width,width), "pix":ppg, "n-rooms":5})
   buffer = io.BytesIO()
   image.save(buffer, format="PNG")
   base64_image = base64.b64encode(buffer.getvalue()).decode("utf-8")
